b6fec028431b60d3c0dcf7dcc44d96e57c99635e
ImaevDmitrii <121541029+ImaevDmitrii@users.noreply.github.com>
2023-02-28 10:29:37 +0300

    Add files via upload

---
+++ EmojiDictionary/EmojiDictionary/emoji.swift
@@ -0,0 +1,16 @@
+//
+//  emoji.swift
+//  EmojiDictionary
+//
+//  Created by Дмитрий Имаев on 26.02.2023.
+//
+
+import Foundation
+
+
+struct Emoji{
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
